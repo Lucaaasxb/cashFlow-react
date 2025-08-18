36182cd4c0472d7dc46743f839e8f40baa2a70fd
Daniel Henrique <lucasbriansilvafagundes@gmail.com>
2025-08-18 18:07:47 -0300

    feat: adicionar sidebard original e integrado com layout

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,22 +4,14 @@ import { Link } from 'react-router-dom'
 function Menu() {
     return (
         <>
-            <nav >
-                <header className="navbar">
-                    <p>CashFlow</p>
-                    <nav>
-                        <Link to="/">Início</Link>
-                        <Link to="/Sobre">Quem Somos</Link>
-                        <Link to="/dashboard">Dashboard</Link>
-                        <Link to="/receitas-despesas">Gastos e Rendas</Link>
-                        <Link to="/metas">Metas</Link>
-                        <Link to="/assistente">Algoritmo Mãe</Link>
-                        <Link to="/personalizacao">Personalização</Link>
-                        <Link to="/login" className="btn">Entrar</Link>
-                    </nav>
-                </header>
-
-            </nav>
+            <header className="navbar">
+                <p>CashFlow</p>
+                <nav>
+                    <Link to="/">Início</Link>
+                    <Link to="/Sobre">Quem Somos</Link>
+                    <Link to="/login" className="btn">Entrar</Link>
+                </nav>
+            </header>
         </>
     )
 }
